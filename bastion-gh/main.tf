6d9d630e27f7bb3a8daef07cf184cf9ec2e1d5b5
Darren Pham <phamdarren1@gmail.com>
2020-12-10 14:41:51 -0800

    bastion github user modules change

--- bastion-gh/main.tf
+++ bastion-gh/main.tf
@@ -3,13 +3,13 @@ locals {
   subnet_id = element(var.public_subnet_ids, 1)
 
   template_file_init = templatefile("${path.module}/user_data.sh", {
-    ssh_user                    = var.ssh_user,
-    github_repo_owner           = var.github_file.github_repo_owner,
-    github_repo_name            = var.github_file.github_repo_name,
-    github_branch               = var.github_file.github_branch,
-    github_filepath             = var.github_file.github_filepath,
-    keys_update_frequency       = var.cron_key_update_schedule,
-    // cron_key_update_schedule    = var.cron_key_update_schedule,
+    ssh_user              = var.ssh_user,
+    github_repo_owner     = var.github_file.github_repo_owner,
+    github_repo_name      = var.github_file.github_repo_name,
+    github_branch         = var.github_file.github_branch,
+    github_filepath       = var.github_file.github_filepath,
+    keys_update_frequency = var.cron_key_update_schedule,
+    keys_update_frequency       = var.keys_update_frequency
     additional_user_data_script = var.additional_user_data_script
   })
 }
