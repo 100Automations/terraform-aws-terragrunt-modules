d705a8ed94d0a82165e5c96d8078fc1f8db99f9e
Darren Pham <phamdarren1@gmail.com>
2020-12-09 18:51:16 -0800

    bastion github user modules change

--- bastion-gh/ssh.sh
+++ bastion-gh/ssh.sh
@@ -43,9 +43,10 @@ head -n $line $KEYS_FILE > $TEMP_KEYS_FILE
 # Pull SSH public keys from Github
 IFS=' ' read -r -a GITHUB_USERNAMES_ARRAY <<$(echo ${github_usernames})
 touch ~/home/$SSH_USER/github_username
-for user in "${GITHUB_USERNAMES_ARRAY[@]}"; do
-    echo $user >> ~/home/$SSH_USER/github_username
-done
+echo ${github_usernames} >> ~/home/$SSH_USER/github_usern
+# for user in "${GITHUB_USERNAMES_ARRAY[@]}"; do
+#     echo $user >> ~/home/$SSH_USER/github_username
+# done
 
 # Move the new authorized keys in place.
 # chown $SSH_USER:$SSH_USER $KEYS_FILE
