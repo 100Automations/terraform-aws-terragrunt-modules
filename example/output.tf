0cf1b1c97dea56f1ddb9e77e4908461cbf8d443d
Darren Pham <phamdarren1@gmail.com>
2021-02-28 18:51:30 -0800

    added custom task execution into github-action user iam:passrole

---
+++ example/output.tf
@@ -0,0 +1,7 @@
+output "access_key_id" {
+  value = module.terraform-aws-terragrunt-modules.access_key_id
+}
+
+output "secret_access_key_id" {
+  value = module.terraform-aws-terragrunt-modules.secret_access_key_id
+}
